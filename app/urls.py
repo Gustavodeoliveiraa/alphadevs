0bf5bcc95e58e455da10c12fb814b9eb969ec781
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-08-21 23:13:03 -0300

    feat: started the creation of shopcart

--- app/urls.py
+++ app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     # path('', include('category.urls')),
     path('', include('product.urls')),
     path('', include('account.urls')),
+    path('', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cart/views.py
+++ cart/views.py
@@ -1,2 +1,15 @@
+from typing import Any
 from django.views.generic import ListView
+from .models import Cart, CartItem
+from product.models import Category
 
+
+class ListCartView(ListView):
+    template_name = 'components/list_cart.html'
+    model = Cart
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+
+        return context
