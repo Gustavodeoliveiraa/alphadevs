92390881c11ad8893be7081644a387efe336f030
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-09-20 01:29:00 -0300

    refactor: fixed url bug

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  add_at_cart
     path('mycart/', views.AddToCartView.as_view(), name='mycart'),
     path('mycart/list', views.CartListView.as_view(), name='cart_list'),
     path(
