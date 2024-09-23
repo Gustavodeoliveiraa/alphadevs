59c30dda1961ddd4d59f41718196ad3885f4a0b4
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-09-22 22:27:46 -0300

    fix: resize images of the products

--- product/models.py
+++ product/models.py
@@ -1,5 +1,8 @@
+import os
+from django.conf import settings
 from django.db import models
 from category.models import Category
+from PIL import Image
 
 
 class Product(models.Model):
@@ -25,3 +28,32 @@ class ProductImage(models.Model):
     images = models.ImageField(
         upload_to='product_image', blank=False, null=False
     )
+
+    @staticmethod
+    def resize_image(image, width=1330):
+        image_full_path = image.path
+        image_pillow = Image.open(image_full_path)
+
+        original_width, original_hight = image_pillow.size
+
+        if original_width < width:
+            image_pillow.close()
+            return
+
+        new_hight = round((width * original_hight) / original_width)
+
+        new_image = image_pillow.resize((width, new_hight), Image.LANCZOS)
+
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=100
+        )
+
+    def save(self, *args, **kwargs):
+        saved = super().save(*args, **kwargs)
+
+        if self.images:
+            self.resize_image(self.images)
+
+        return saved
